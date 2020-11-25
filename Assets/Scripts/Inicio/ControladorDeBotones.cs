609be64f68898a686af9b070dd5a61c7d2980360
Luis Plata <www.luisplata@gmail.com>
2020-11-24 19:31:25 -0500

    Toda la interfaz realizada, ahora comienza la logica de todo

---
+++ Assets/Scripts/Inicio/ControladorDeBotones.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ControladorDeBotones : MonoBehaviour
+{
+    [SerializeField] private Button boton;
+    [SerializeField] private int escenaCargar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boton.onClick.AddListener(delegate { CargarEscena(escenaCargar); });
+    }
+    public void CargarEscena(int escenaIndex)
+    {
+        SceneManager.LoadScene(escenaIndex);
+    }
+}
