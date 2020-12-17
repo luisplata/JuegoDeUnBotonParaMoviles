a4b7c02366d267ee6973cc31d0a3a2f37ecac1fa
Luis Plata <www.luisplata@gmail.com>
2020-12-17 14:32:19 -0500

    agregando asembly y vista de la escena de informacion; corrigiendo algunos errores de musica

---
+++ Assets/Scripts/Informacion/ControladorDeEscenarioInformacion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorDeEscenarioInformacion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ServiceLocator.Instance.GetService<IManejadorDeMusica>().ComenzarMusicaCompleta();
+    }
+}
